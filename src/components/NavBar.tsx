f3ee6e3c99055cfb7ec18539c163f2e6ccf2d7c9
ikrishagarwal <68506145+ikrishagarwal@users.noreply.github.com>
2025-11-16 22:33:44 +0530

    feat: added landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 export function NavBar() {
   return (
-    <header className="flex text-slate-50 border-b border-b-gray-border px-6 py-6 items-center">
+    <header className="flex text-slate-50 border-b border-b-gray-border px-6 py-6 items-center sticky">
       <div className="text-2xl font-brand">krish</div>
 
       <nav className="flex-1 flex justify-center gap-4 font-spline-mono uppercase">
