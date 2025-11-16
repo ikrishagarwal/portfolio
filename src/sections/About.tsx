d67e5f5e29906657af1d2f9bee09c53443df4cad
ikrishagarwal <68506145+ikrishagarwal@users.noreply.github.com>
2025-11-16 23:59:35 +0530

    feat: added about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,15 +1,25 @@
 export function About() {
   return (
-    <section className="min-h-screen w-full bg-background text-slate-50 flex flex-col items-center justify-center px-6">
-      <h1 className="text-4xl font-bold mb-4">About Me</h1>
-      <p className="max-w-2xl text-center text-lg leading-relaxed">
-        Hello! I'm Krish, a passionate developer with a love for creating
-        beautiful and functional web applications. With a strong background in
-        JavaScript and React, I enjoy building user-friendly interfaces and
-        exploring new technologies. In my free time, I like to contribute to
-        open-source projects and learn about the latest trends in web
-        development.
-      </p>
+    <section className="min-h-screen w-full bg-background text-amber-50 px-8 py-20 border-b border-gray-border">
+      <h1 className="text-8xl font-bold mb-4 font-brand text-center">
+        About Me
+      </h1>
+      <div className="max-w-4xl mx-auto mt-20 text-justify text-2xl leading-relaxed font-spline-mono font-regular">
+        <p className="mb-6">
+          Hello! I'm Krish, a passionate developer with a love for creating
+          beautiful and functional web applications. With a strong background in
+          JavaScript and React, I enjoy building user-friendly interfaces and
+          exploring new technologies. In my free time, I like to contribute to
+          open-source projects and learn about the latest trends in web
+          development.
+        </p>
+        <p>
+          I have worked in various full-stack projects, mobile applications,
+          games, bot and automation scripts, and even hardware projects like
+          mechanical keyboards. My goal is to continuously improve my skills and
+          contribute to meaningful projects that make a difference.
+        </p>
+      </div>
     </section>
   );
 }
